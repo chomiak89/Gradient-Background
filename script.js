a9fbc8ade448927948a12083440819ad7a44e04b
Kamil <kchomiak@outlook.com>
2019-08-09 00:35:19 -0500

    Refactored entire js code, added random colors

--- script.js
+++ script.js
@@ -4,67 +4,67 @@ To add:
 -button to change the gradient from left-right to top-down
 */
 
-
 let primary = document.querySelector("#primary");
 let secondary = document.querySelector("#secondary");
 let body = document.getElementsByTagName("body");
 let colorTextDisplay = document.querySelector("#colorTextDisplay");
-let gradientDirectionSelection = document.querySelector("#gradientDirectionSelection");
+let gradientDirectionSelection = document.querySelector(
+  "#gradientDirectionSelection"
+);
+const randomButton = document.querySelector("#randomColors");
 
-//VARIABLES
-let gradientDirection = "to right";
+let variables = {
+  color1: "rgb(73, 241, 116)",
+  color2: "rgb(115, 165, 247)",
+  direction: "to right"
+};
 
-//Create and set the inital values for primary and secondary color variables
-let primaryColorPicked = {r: 73, g: 241, b: 116}; 
-let secondaryColorPicked = {r: 115, g: 165, b: 247};
-
+const createRgb = obj => {
+  return `rgb(${obj.r}, ${obj.g}, ${obj.b})`;
+};
 
 //EVENT LISTENERS ===================================================================================
 //Event listener for primary "left" color
-primary.addEventListener("input", function(){
-    primaryColorPicked = hexToRgb(primary.value);
-    changeBackgroundColor();
-})
+primary.addEventListener("input", function() {
+  variables.color1 = createRgb(hexToRgb(primary.value));
+  changeBackgroundColor();
+});
 //Event listener for secondary "right" color
-secondary.addEventListener("input", function(){
-    secondaryColorPicked = hexToRgb(secondary.value);
-    changeBackgroundColor();
-})
+secondary.addEventListener("input", function() {
+  variables.color2 = createRgb(hexToRgb(secondary.value));
+  changeBackgroundColor();
+});
 //Event listener for gradient direction
 gradientDirectionSelection.addEventListener("input", () => {
-    gradientDirection = gradientDirectionSelection.value;
-    changeBackgroundColor();
-})
+  variables.direction = gradientDirectionSelection.value;
+  changeBackgroundColor();
+});
+randomButton.addEventListener("click", () => {
+  variables.color1 = randomizeColor();
+  variables.color2 = randomizeColor();
+  changeBackgroundColor();
+});
 
-
-//FUNCTIONS =========================================================================================
-//Builds the primary part of the rgba declaration
-const buildPrimary = () => {
-    let color = `rgb(${primaryColorPicked.r}, ${primaryColorPicked.g}, ${primaryColorPicked.b})`;
-    return color;
-}
-//Builds the secondary part of the rgba declaration
-const buildSecondary = () => {
-    let color = `rgb(${secondaryColorPicked.r}, ${secondaryColorPicked.g}, ${secondaryColorPicked.b})`;
-    return color;
-}
-//Builds the entire rgba declaration using buildPrimary() and buildSecondary()
-const buildRgbColor = () => {
-    let pri = `linear-gradient(${gradientDirection}, ${buildPrimary()}, ${buildSecondary()})`;
-    return pri;
-}
 //Changes the style attribute of body to change the color, alse updates inner text of the h4 to display current values
 const changeBackgroundColor = () => {
-    body[0].style.background = buildRgbColor();
-    colorTextDisplay.innerText = buildRgbColor();
-}
-
-
-
+  body[0].style.background = `linear-gradient(${variables.direction}, ${
+    variables.color1
+  }, ${variables.color2})`;
+  colorTextDisplay.innerText = `linear-gradient(${variables.direction}, ${
+    variables.color1
+  }, ${variables.color2})`;
+};
+const randomizeColor = () => {
+  colorObject = { r: 0, g: 0, b: 0 };
+  Object.keys(colorObject).forEach(el => {
+    colorObject[el] = Math.round(Math.random() * 255);
+  });
+  return `rgb(${colorObject.r}, ${colorObject.g}, ${colorObject.b})`;
+};
 
 //CODE TO CONVERT HEX TO RGB ===========================================================================
 /**
- * hexToRgb 
+ * hexToRgb
  * @param  string hex Hexadicimal value
  * @return number     description
  * @usage  //alert(hexToRgb("#0033ff").g); // "51"
@@ -73,16 +73,18 @@ const changeBackgroundColor = () => {
  * @usage  //alert(hexToRgb("03f").g);     // "51"
  */
 function hexToRgb(hex) {
-	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-	hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-		return r + r + g + g + b + b;
-	});
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
 
-	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	return result ? {
-		r: parseInt(result[1], 16),
-		g: parseInt(result[2], 16),
-		b: parseInt(result[3], 16)
-	} : null;
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      }
+    : null;
 }
